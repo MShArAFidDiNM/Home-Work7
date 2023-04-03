70f34e65ca9683821235de8bd1f479df94b4d946
MShArAFidDiNM <sharafiddin2007@outlook.com>
2023-04-03 16:20:58 +0500

    Task_1

--- Home_work_7/Home_work_7/Program.cs
+++ Home_work_7/Home_work_7/Program.cs
@@ -10,6 +10,61 @@ namespace Home_work_7
     {
         static void Main(string[] args)
         {
+
+            Random rand = new Random();
+            int randnumber = rand.Next(0, 10);
+            int[][] arr = new int[randnumber][];
+
+            for (int i = 0; i < randnumber; i++)
+            {
+                int rand_num2 = rand.Next(3, 10);
+                arr[i] = new int[rand_num2];
+
+                Console.WriteLine($"[{randnumber}][{rand_num2}]");
+            }
+
+            Max(arr);
+
+            Console.ReadLine();
+        }
+
+        public static void Max(int[][] arr)
+        {
+
+            Random rand = new Random();
+
+            int max = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int currentMax = 0;
+                int sum = 0;
+
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    arr[i][j] += rand.Next(1, 10);
+
+                    Console.Write($"{arr[i][j]}  ");
+
+                    currentMax += arr[i][j];
+                    sum += arr[i][j];
+
+                }
+
+
+                if (currentMax > max)
+                {
+                    max = currentMax;
+                }
+
+
+                Console.WriteLine($" : sum : {sum}");
+
+            }
+
+            Console.WriteLine($"Max : {max}");
+
         }
     }
+    
 }
